8739671d11c2b390a8eb3438a6f123fa34c0d09c
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-04-16 14:20:30 +0530

    adding day-1 project band name generator

---
+++ Beginner/Day 1/Project/band-name-generator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator.')
+
+city = input('Whats the name of city you grew up in? \n').title()
+pets_name = input('What\'s your pets name? \n').title()
+
+print('Your band name could be ' + city + ' ' + pets_name)
